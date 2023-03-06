dcef4354f8042afeca9c55d5358dc0832b258241
EniuK <eniuk113@gmail.com>
2023-03-06 10:45:54 +0100

    add styles to news component

--- app/screens/Home/RecentNewsFragment.tsx
+++ app/screens/Home/RecentNewsFragment.tsx
@@ -5,34 +5,31 @@ import CardCar from "../../../assets/images/card_car.png"
 const RecentNewsFragment = () => {
   return (
     <VStack space="4">
-      <Text bold fontSize="xs">
+      <Text bold fontSize="2xl">
         Recent News
       </Text>
-      <FlatList scrollEnabled  data={fakeArticles} renderItem={Article} />
+      <FlatList scrollEnabled data={fakeArticles} renderItem={Article} />
     </VStack>
   )
 }
 const fakeArticles = [
   {
     id: 1,
-    title: "React Native",
+    title: "New collection arrived!",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum laudantium fugiat tenetur minima in reprehenderit suscipit eveniet! Fugiat, beatae, voluptates atque harum praesentium deleniti modi similique eligendi ut, odit iusto.",
     category: "cat",
- 
   },
   {
     id: 2,
     title: "Matchboxes on the way",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum laudantium fugiat tenetur minima in reprehenderit suscipit eveniet! Fugiat, beatae, voluptates atque harum praesentium deleniti modi similique eligendi ut, odit iusto.",
     category: "cat",
- 
   },
   {
     id: 3,
     title: "Diecasters are on high",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum laudantium fugiat tenetur minima in reprehenderit suscipit eveniet! Fugiat, beatae, voluptates atque harum praesentium deleniti modi similique eligendi ut, odit iusto.",
     category: "cat",
- 
   },
   {
     id: 4,
@@ -44,17 +41,19 @@ const fakeArticles = [
 
 function Article({ item: { title, desc, category } }) {
   return (
-    <VStack paddingBottom="3"  mb={5} borderTopLeftRadius={20} borderTopRightRadius={20} bg="coolGray.300" width={"100%"} >
-      
+    <VStack p={3} mb={5} borderRadius={20} bg="coolGray.300" width={"100%"}>
       <Box position={"relative"} flex={1}>
-        <Image source={CardCar} borderTopLeftRadius={20} borderTopRightRadius={20}  alt="big red kitty" />
-        <Text position={"absolute"} bottom={0} bold fontSize="xl" color={"white"} bg={"black"} padding={2}>{category}</Text>
-
+        <Image source={CardCar} borderRadius={8} alt="big red kitty" />
       </Box>
-      <Text bold marginTop={0} marginBottom={5} paddingLeft={5}  fontSize="2xl">
+      <Text bold marginTop={0} marginBottom={3} fontSize="2xl">
         {title}
       </Text>
-      <Text paddingLeft={5} bg="red.600" numberOfLines={5}>{desc}</Text>
+      <Text numberOfLines={3} fontSize={"lg"} pr={2}>
+        {desc}
+      </Text>
+      <Text bg={"gray.400"} mt={3} w={20} alignSelf={"flex-end"} mr={3} mb={1}>
+        Read more
+      </Text>
     </VStack>
   )
 }
