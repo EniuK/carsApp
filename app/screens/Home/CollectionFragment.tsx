2e0c3ed08b2b305c430b3cd414b89b04f0138975
EniuK <eniuk113@gmail.com>
2023-03-08 12:06:49 +0100

    change collection styles and add working filter

--- app/screens/Home/CollectionFragment.tsx
+++ app/screens/Home/CollectionFragment.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Text, Box, VStack, Image, FlatList, Input, HStack, ArrowBackIcon } from "native-base"
 import CardCar from "../../../assets/images/card_car.png"
 import car1 from "../../../assets/images/car1.jpeg"
@@ -6,22 +6,28 @@ import car2 from "../../../assets/images/car2.jpeg"
 import car3 from "../../../assets/images/car3.jpeg"
 
 const CollectionFragment = () => {
+  const [filter, setFilter] = useState("")
+
+  const filteredCars = fakeCarCollection.filter((car) => {
+    return car.name.toLowerCase().includes(filter.toLowerCase())
+  })
+
   return (
     <VStack space="4">
       <HStack w="100%">
         <ArrowBackIcon size="5" mt={2} mr={2} color="emerald.500" />
-        <Input size="lg" placeholder="search car" w="80%" />
+        <Input
+          onChangeText={(text) => setFilter(text)}
+          size="lg"
+          placeholder="search car"
+          w="80%"
+        />
       </HStack>
-      <Text borderStyle="solid" borderColor={"black"} borderWidth={1} w={100} ml={203}>
-        Sorting icon
+      <Text bold w={100}>
+        All ({filteredCars.length})
       </Text>
       <HStack>
-        <FlatList
-          scrollEnabled
-          numColumns={2}
-          data={fakeCarCollection}
-          renderItem={CollectionItem}
-        />
+        <FlatList scrollEnabled numColumns={2} data={filteredCars} renderItem={CollectionItem} />
       </HStack>
     </VStack>
   )
@@ -30,37 +36,44 @@ const fakeCarCollection = [
   {
     id: 1,
     src: CardCar,
+    name: "Citroen",
   },
   {
     id: 2,
 
     src: car1,
+    name: "dodge",
   },
   {
     id: 3,
 
     src: car2,
+    name: "red something",
   },
   {
     id: 4,
 
     src: car3,
+    name: "green truck",
   },
 ]
 
-function CollectionItem({ item: { src } }) {
+function CollectionItem({ item: { src, name } }) {
   return (
-    <Box borderRadius={10} w={"50%"} mr={5} mb={5} ml={0}>
-      <Image
-        source={src}
-        borderRadius={10}
-        width={150}
-        h={150}
-        alt="big red kitty"
-        bg="black"
-        pr={0}
-        ml={0}
-      />
+    <Box borderRadius={10} justifyContent={"space-between"} w={"50%"} mb={5}>
+      <VStack>
+        <Image
+          source={src}
+          borderRadius={10}
+          width={150}
+          h={150}
+          alt="big red kitty"
+          bg="black"
+          pr={0}
+          ml={0}
+        />
+        <Text bold>{name}</Text>
+      </VStack>
     </Box>
   )
 }
