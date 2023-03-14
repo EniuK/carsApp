0f912806533f10aa24cce748c09ed6871633af45
EniuK <eniuk113@gmail.com>
2023-03-15 00:13:46 +0100

    change styles to addCar component

--- app/screens/AddCar.tsx
+++ app/screens/AddCar.tsx
@@ -1,41 +1,56 @@
-import { ViewStyle } from "react-native"
-import React from "react"
-import {
-  Text,
-  Box,
-  VStack,
-  Image,
-  HStack,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  ScrollView,
-  Divider,
-  FlatList,
-  Input,
-  Button,
-} from "native-base"
+import { SafeAreaView, ViewStyle } from "react-native"
+import React, { useCallback } from "react"
+import { Text, Box, VStack, Image, HStack, ScrollView, Divider, Input } from "native-base"
 import BackHeader from "../components/BackHeader"
-import { AntDesign } from "@expo/vector-icons"
 import CardCar from "../../assets/images/card_car.png"
-import ScreenWrapper from "../components/ScreenWrapper"
 import Buttonv2 from "../components/Buttonv2"
+import LinearGradient from "react-native-linear-gradient"
+import Touchable from "../components/Touchable"
+import { AntDesign } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native"
 
 const AddCar = () => {
   const colors = ["red", "white", "blue"]
-
+  const navigation = useNavigation()
+  const onBack = useCallback(() => {
+    navigation.goBack()
+  }, [])
   return (
-    <ScreenWrapper>
-      <BackHeader title="ModelDetails" />
+    <Box>
+      <LinearGradient
+        start={{ x: 0, y: 1 }}
+        end={{ x: 2, y: 0 }}
+        colors={["#06153C", "#2917FC", "#192f6a"]}
+      >
+        <SafeAreaView>
+          <BackHeader hideHeader title="" />
 
-      <ScrollView>
-        <Box ml={9} mr={9} pt={5} pb={5} borderRadius={5}>
-          <HStack justifyContent={"flex-start"} alignItems={"flex-start"}>
-            <Box mb={3} w={"40%"} justifyContent={"center"} alignItems={"center"}>
-              <Text bold fontSize={"xl"}>
+          <VStack mt={-9} justifyContent={"flex-start"} alignItems={"flex-start"} mb={4}>
+            <Box
+              ml={4}
+              mb={2}
+              width={"30%"}
+              justifyContent={"flex-start"}
+              alignItems={"flex-start"}
+            >
+              <Box style={$iconContainer}>
+                <Touchable onPress={onBack}>
+                  <AntDesign name="arrowleft" size={28} color="white" />
+                </Touchable>
+              </Box>
+            </Box>
+            <Box mb={3} w={"40%"} ml={4} justifyContent={"flex-start"} alignItems={"flex-start"}>
+              <Text bold color={"white"} fontSize={"18"}>
                 Add new
               </Text>
             </Box>
-          </HStack>
+          </VStack>
+        </SafeAreaView>
+      </LinearGradient>
+
+      <ScrollView>
+        <Box pb={270} borderRadius={5} bg={"#FFFFFF"}>
+          {/* padding to fix */}
           <VStack bg={"#FFFFFF"}>
             <Box justifyContent={"center"} alignItems={"center"}>
               <Image
@@ -45,17 +60,21 @@ const AddCar = () => {
                 w="100%"
                 alt="big red kitty"
               />
-              <Text
-                position={"absolute"}
-                justifyContent={"center"}
-                alignItems={"center"}
-                bold
-                fontSize={"xl"}
-              >
-                Add photo
-              </Text>
+              <Box position={"absolute"} justifyContent={"center"} alignItems={"center"} w={"70%"}>
+                <Text
+                  bold
+                  fontSize={"xl"}
+                  pt={2}
+                  pb={2}
+                  pr={20}
+                  pl={20}
+                  style={{ borderWidth: 1, borderColor: "blue" }}
+                >
+                  Add photo
+                </Text>
+              </Box>
             </Box>
-            <Box p={10} pt={5} justifyContent={"center"} pb={20}>
+            <Box pl={16} pr={16} pt={4} justifyContent={"center"}>
               <Box mt={2} w={"100%"}>
                 <Input type="text" borderWidth={0} pl={0} />
                 <Divider w={"100%"} bg={"#B7B7B7"} />
@@ -81,7 +100,7 @@ const AddCar = () => {
               </Box>
               <Box mt={2}>
                 <Input type="text" borderWidth={0} pl={0}></Input>
-                <Divider w={"25%"} bg={"#B7B7B7"} />
+                <Divider w={"100%"} bg={"#B7B7B7"} />
                 <Text color={"#B7B7B7"} fontSize={"xs"}>
                   YEAR
                 </Text>
@@ -93,7 +112,7 @@ const AddCar = () => {
                   ID NUMBER
                 </Text>
               </Box>
-              <Box mt={2}>
+              <Box mt={8}>
                 <HStack space={2}>
                   <Box bg={colors[0] + ".600"} style={$shadow} w={5} h={5} borderRadius={30} />
                   <Box bg={colors[1]} style={$shadow} w={5} h={5} borderRadius={30} />
@@ -113,7 +132,7 @@ const AddCar = () => {
               <Box mt={2} mb={5}>
                 <Input type="text" borderWidth={0} pl={0}></Input>
                 <Divider bg={"#B7B7B7"} />
-                <Text color={"#B7B7B7"} fontSize={"xs"}>
+                <Text color={"#B7B7B7"} fontSize={"xs"} mb={7}>
                   DATE ADDED
                 </Text>
               </Box>
@@ -122,9 +141,10 @@ const AddCar = () => {
           </VStack>
         </Box>
       </ScrollView>
-    </ScreenWrapper>
+    </Box>
   )
 }
+
 const $shadow: ViewStyle = {
   shadowColor: "#000",
   shadowOffset: {
@@ -135,5 +155,12 @@ const $shadow: ViewStyle = {
   shadowRadius: 3.84,
 
   elevation: 5,
+}
+const $iconContainer: ViewStyle = {
+  padding: 5,
+  marginBottom: 2,
+  marginRight: 2,
+  borderRadius: 20,
+  backgroundColor: "#09154d",
 }
 export default AddCar
